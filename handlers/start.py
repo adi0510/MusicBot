e3a89751b6c85b479808b063ac900ffc02a2eba9
adi0510 <80454994+adi0510@users.noreply.github.com>
2021-03-15 12:26:23 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -16,7 +16,7 @@ from helpers.wrappers import errors, admins_only
 async def start_(client: Client, message: Message):
     await message.reply_text(
        f"""🙃 Hi {message.from_user.first_name}!
-✨ I am Hêllẞø† Music Player. 
+✨ I am HELLBOTxADI Music Player. 
 🥳 I can play music in your Telegram Group's Voice Chat😉
 ⚜️ Use these buttons below to know more. 👇""",
         reply_markup=InlineKeyboardMarkup(
@@ -51,7 +51,7 @@ async def start_(client: Client, message: Message):
 )
 async def start(client: Client, message: Message):
     await message.reply_text(
-        "**Hêllẞø†:** I'm Working!!!\nUse me in Inline to search for a YouTube Video/Music. \n**Happy Streaming**",
+        "**HELLBOTxADI:** I'm Working!!!\nUse me in Inline to search for a YouTube Video/Music. \n**Happy Streaming**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
